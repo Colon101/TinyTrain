2687c673e53dc760b39a44fcdc6e25d64e82fd72
Colon101 <kfirgoldman12@gmail.com>
2026-04-27 10:50:35 +0300

    Add @vercel/analytics dependency and integrate analytics injection in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,11 @@
 	let { children } = $props();
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	
 	injectSpeedInsights();
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 {@render children()}
